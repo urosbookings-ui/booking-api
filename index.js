8c20402edf94c19665e42eeed0f549b18f74a428
urosbookings-ui <uros.bookings@gmail.com>
2025-11-10 19:03:19 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ ok: true, msg: "API radi na Vercelu 🚀" });
+});
+
+export default app;
