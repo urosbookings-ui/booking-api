a08bcafdc473d280e2abfa05f8689637dd922181
urosbookings-ui <uros.bookings@gmail.com>
2025-11-22 12:09:19 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -78,7 +78,8 @@ export default async function handler(req, res) {
     }
     
     // Vraća se greška 404 samo ako je 'action' definisan ali nije prepoznat
-    return res.status(404).json({ ok: false, error: "Unknown action" });
+    // ...
+    return res.status(404).json({ ok: false, error: "OVO JE NOVA VERZIJA KODA, ALI AKCIJA NIJE PREPOZNATA" });
   }
 
   // --- POST REQUEST (CREATE BOOKING) ---
